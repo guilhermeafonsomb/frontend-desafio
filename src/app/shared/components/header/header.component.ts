2634cd4d0eab28ec6f26ed89046927509fbfe011
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-03-02 16:23:57 -0300

    wip: saving user on localStorage

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { UsersService } from '../../../core/services/users/users.service';
+import { IUser } from '../../models/users.interface';
 
 @Component({
   selector: 'app-header',
@@ -15,4 +16,8 @@ export class HeaderComponent {
   constructor(private usersService: UsersService) {}
 
   users$ = this.usersService.getAllUsers();
+
+  handleUser(user: IUser) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
 }
